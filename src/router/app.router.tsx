0b6dcf1b5b523d83d8ac887e832ffe3170843f9b
d4mg <diegoamarin2498@gmail.com>
2025-12-18 15:54:29 -0500

    Primer commit del proyecto, dejamos las bases listas

---
+++ src/router/app.router.tsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router";
+import { HomePage } from "@/heroes/page/home/HomePage";
+import { HeroPage } from '@/heroes/page/hero/HeroPage';
+import { SearchPage } from "@/heroes/page/search/SearchPage";
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+
+export const appRouter = createBrowserRouter([
+    {
+        path:'/',
+        element: <HeroesLayout/>,
+        children: [
+            {
+                index: true,
+                element: <HomePage/>,
+            },
+            {
+                path:'heroes/1',
+                element: <HeroPage/>,
+            },
+            {
+                path: 'search',
+                element: <SearchPage/>,
+            },
+        ],
+    },
+
+    {
+        path:'/admin',
+        element: <AdminLayout/>,
+        children:[
+
+            {
+                index: true,
+                element: <AdminPage/>,
+            },
+        ]
+    },
+]);
